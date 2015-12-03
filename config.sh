c2ac156219fe90f1b89df56df401a7f3fdadb05e
Brad Nelson <bnelsj@gmail.com>
2015-12-03 14:27:03 -0800

    Added tabix module

--- config.sh
+++ config.sh
@@ -5,4 +5,4 @@ module load modules modules-init modules-gs/prod modules-eichler/prod
 module load samtools/1.2
 module load VariationHunter/0.4
 module load anaconda/2.1.0
-
+module load tabix/latest
